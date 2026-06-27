5b049fee8d53e97c4deaf67c29eff018634e4fea
Tauhidul <isakib49@gmail.com>
2026-06-27 20:01:39 +0600

    added supabase to the homepage

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,55 +2,24 @@ import { GiftCardProps } from "@/components/GiftCard";
 import GiftCarousel from "@/components/GiftCarousel";
 import Hero from "@/components/Hero";
 import HeroText from "@/components/HeroText";
-export default  function Home() {
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+export default async  function Home() {
+
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+ const { data: gifts } = await supabase.from('gifts').select()
 
 
 
-  const GIFT_ITEMS: GiftCardProps[] = [
-  {
-    category: "Premade Box",
-    title: "Let's Make More Memories",
-    imageSrc: "https://i.ibb.co.com/S74vPxF/Rectangle-14.png",
-    imageAlt: "Instax camera, teddy bear and chocolate gift box",
-  },
-  {
-    category: "Premade Box",
-    title: "Friends For Life",
-    imageSrc: "/images/friends.jpg",
-    imageAlt: "Certificate of Appreciation with polaroid photos",
-  },
-  {
-    category: "Premade Box",
-    title: "Kain Sarimbit Jentik Manis Tanjung Merah Bata",
-    imageSrc: "/images/batik.jpg",
-    imageAlt: "Folded batik fabric in rich terracotta tones",
-  },
-  {
-    category: "Box & Tale x Sababay",
-    title: "Story (Mistelle)",
-    imageSrc: "/images/story.jpg",
-    imageAlt: "Wine bottle with fresh flowers in a wooden box",
-  },
-  {
-    category: "Premade Box",
-    title: "Sweet Celebration",
-    imageSrc: "/images/celebration.jpg",
-    imageAlt: "Celebration gift box with sweets",
-  },
-  {
-    category: "Custom Box",
-    title: "Heartfelt Gratitude",
-    imageSrc: "/images/gratitude.jpg",
-    imageAlt: "Gratitude gift box",
-  },
-];
+
   return (
     <div className="">
      <Hero/>
 
         <HeroText/>
       <GiftCarousel
-      items={GIFT_ITEMS} />
+      items={gifts ?? []} />
 
    <div>
  
