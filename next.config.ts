b736b58336910280271469e344b54fe5f76d8ca6
Tauhidul <isakib49@gmail.com>
2026-08-18 22:46:19 +0600

    fix: remove leading space from i.ibb.co.com hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: " i.ibb.co.com",
+        hostname: "i.ibb.co.com",
       },
       {
         protocol: "https",
